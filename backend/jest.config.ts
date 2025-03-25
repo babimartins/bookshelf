eabc11535dd6ff29f4fe50198f56f0823d3674e1
Bárbara Martins <martins.barbara04@gmail.com>
2025-03-25 01:11:47 -0300

    Add unit tests config

---
+++ backend/jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "jest";
+
+const config: Config = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    verbose: true,
+    testMatch: ["**/__tests__/**/*.test.ts"],
+    moduleFileExtensions: ["ts", "js"],
+    transform: {
+        "^.+\\.ts$": "ts-jest",
+    },
+};
+
+export default config;
---
+++ frontend/cypress/support/e2e.ts
@@ -0,0 +1,3 @@
+import "@testing-library/cypress/add-commands";
+
+Cypress.on("uncaught:exception", () => false);
