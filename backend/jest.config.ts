c823139f162640f63b7d7538638775f7c29fd2c9
Bárbara Martins <martins.barbara04@gmail.com>
2025-04-07 01:24:44 -0300

    Improve jest configs

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -4,10 +4,33 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js'],
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': ['ts-jest', {}],
+  },
+
+  clearMocks: true,
+
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/index.ts',
+    '!src/config/**',
+    '!src/types/**',
+    '!src/**/*.d.ts',
+    '!src/**/I*.{ts,js}',
+    '!**/node_modules/**',
+  ],
+  moduleNameMapper: {
+    '^#config/(.*)$': '<rootDir>/src/config/$1',
+    '^#models/(.*)$': '<rootDir>/src/models/$1',
+    '^#routes/(.*)$': '<rootDir>/src/routes/$1',
+    '^#services/(.*)$': '<rootDir>/src/services/$1',
+    '^#middlewares/(.*)$': '<rootDir>/src/middlewares/$1',
+    '^#utils/(.*)$': '<rootDir>/src/utils/$1',
   },
 };
 
